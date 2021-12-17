94dc2d8688343a21834d2d006de898c3a0614b08
anaya-che <anaya.che22@gmail.com>
2021-12-17 23:56:17 +0300

    feat: add app module

---
+++ christmas-task/src/components/app.ts
@@ -0,0 +1,30 @@
+import { IToy } from '../types/types';
+import ToysPage from './toysPage';
+import Filter from './filter';
+
+class App {
+  data: IToy[];
+
+  constructor() {
+    this.data = [];
+  }
+
+  async getAllToys() {
+    const toysData = '../data/data.json';
+    const res: Response = await fetch(toysData);
+    this.data = await res.json();
+  }
+
+  getToysPage() {
+    ToysPage.render();
+    const filter: Filter = new Filter(this.data);
+    filter.start();
+  }
+
+  start() {
+    this.getAllToys();
+    this.getToysPage();
+  }
+}
+
+export default App;
