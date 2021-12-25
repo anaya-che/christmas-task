6f8173c6f4ecd19669c28686f663e074dc0fdc42
anaya-che <anaya.che22@gmail.com>
2021-12-25 18:48:44 +0300

    feat: add page navigation

--- christmas-task/src/components/app.ts
+++ christmas-task/src/components/app.ts
@@ -1,6 +1,7 @@
 import { IToy } from '../types/types';
 import MainPage from './mainPage/mainPage';
 import ToysPage from './toys/toysPage/toysPage';
+import TreePage from './treePage/treePage';
 import Options from './toys/filter/options';
 import Slider from './toys/slider/slider';
 
@@ -15,6 +16,7 @@ class App {
     const toysData = '../data/data.json';
     const res: Response = await fetch(toysData);
     this.data = await res.json();
+    document.addEventListener('click', this.navigation.bind(this));
   }
 
   static createImage = (src: string): Promise<unknown> => new Promise((res, rej): void => {
@@ -35,10 +37,45 @@ class App {
     MainPage.render();
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  getTreePage(): void {
+    TreePage.render();
+  }
+
   async start(): Promise<void> {
     await this.getAllToys();
-    // this.getToysPage();
     App.getMainPage();
+  }
+
+  navigation(event: MouseEvent): void {
+    const target = <HTMLElement>event.target;
+    if (target.closest('.nav-main')) {
+      App.removeActiveClass();
+      App.getMainPage();
+    }
+    if (target.closest('.main__button')
+      || target.closest('.nav-toys')
+    ) {
+      App.addActiveClass('.nav-toys');
+      this.getToysPage();
+    }
+    if (target.closest('.nav-tree')) {
+      App.addActiveClass('.nav-tree');
+      this.getTreePage();
+    }
+  }
+
+  static addActiveClass(className: string): void {
+    App.removeActiveClass();
+    const target = <HTMLElement>document.querySelector(className);
+    target.classList.add('active-link');
+  }
+
+  static removeActiveClass(): void {
+    const allNavItems: NodeListOf<HTMLElement> = document.querySelectorAll('.header__nav-item');
+    allNavItems.forEach((el: HTMLElement): void => {
+      if (el.classList.contains('active-link')) el.classList.remove('active-link');
+    });
   }
 }
 
